f0784c87946e4a9ddbbed2831b75a762e741465f
yitian1808 <yitian1808@163.com>
2018-03-11 09:59:34 +0800

    tangyansheng

--- pages/login/login.js
+++ pages/login/login.js
@@ -36,17 +36,22 @@ Page({
       // 这里修改成跳转的页面  
       var header = getApp().globalData.header; //获取app.js中的请求头
       wx.request({
-        url: 'https://www.hems999.com/weixinLogin', //仅为示例，并非真实的接口地址
+        url: 'https://www.hems999.com/weixinSmall!weixinLogin', //仅为示例，并非真实的接口地址
         data: {},
         header: header,
         success: function (res) {
-          console.log("从app.js请求服务器test.php获取数据");
           var query_clone = res.data;
           console.log(query_clone);
-          this.setData({
+          if (query_clone.flg == '0') {
+              wx.showToast({
+                title: query_clone.message,
+                icon: 'loading',
+                duration: 2000
+              })
+            } else{
+              //登陆成功跳转
 
-          });
-
+            }
         }
       });
     }
