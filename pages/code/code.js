cbe0fb27c1054dfba3ec2b7ebc3385c9a78df31d
songxiaoqing2018 <songxiaoqing@mitgroup.com.cn>
2018-04-03 17:02:47 +0800

    song

---
+++ pages/code/code.js
@@ -0,0 +1,84 @@
+// pages/code/code.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    beginDate:'',
+    endDate:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'https://www.hems999.com/weixinSmall!addMember', //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log("获取产品生效日期");
+        var proDate = res.data;
+        console.log(res.data);
+        that.setData({ 
+          beginDate: proDate,
+          endDate: proDate,
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.setNavigationBarTitle({
+      title: '九九九空中救护'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
