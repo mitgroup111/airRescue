f934fea08d462b8e66c1bb544bf3b6b7b49e2d81
songxiaoqing2018 <songxiaoqing@mitgroup.com.cn>
2018-04-18 10:18:07 +0800

    song

--- pages/user/user.js
+++ pages/user/user.js
@@ -2,14 +2,108 @@
 //获取应用实例
 var flg;
 const app = getApp()
-
+var appInstance = getApp();
 Page({
   data: {
     haslogin: false,
+    phone: '',
+    password: ''
   },
-  onLoad: function (options) {
+  // 获取输入手机号  
+  phoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
   },
 
+  // 获取输入密码  
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+  onLoad: function (options) {
+    this.WxValidate = appInstance.wxValidate(
+      {
+        mobile: {
+          required: true,
+          tel: true,
+        },
+        password: {
+          required: true,
+          minlength: 6,
+          maxlength: 20,
+        }
+      },
+      {
+        mobile: {
+          required: '请输入手机号',
+          tel: '请输入正确的手机号'
+        },
+        password: {
+          required: '请输入密码',
+          minlength: '密码至少为6位',
+          maxlength: '密码至多为20位',
+        }
+      }
+    )
+  },
+  // 登录  
+  formSubmit: function (e) {
+    var that=this;
+    if (!this.WxValidate.checkForm(e)) {
+      const error = this.WxValidate.errorList[0]
+      // `${error.param} : ${error.msg} `
+      wx.showModal({
+        content: `${error.msg} `,
+        duration: 2000
+      })
+      return false
+    } else {
+      // 这里修改成跳转的页面  
+      var value = wx.getStorageSync('sessionId');
+      console.log("value:" + value);
+      console.log("userName:" + e.detail.value.mobile);
+      console.log("password:" + e.detail.value.password);
+      wx.request({
+        url: 'https://teach.hems999.com/weixinSmall!weixinLogin', //仅为示例，并非真实的接口地址
+        data: {
+          "sessionId": value,
+          "userName": e.detail.value.mobile,
+          "password": e.detail.value.password
+        },
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          var query_clone = res.data[0];
+          console.log(query_clone);
+          if (query_clone.flg == '0') {
+            wx.showModal({
+              content: query_clone.message,
+              duration: 2000
+            })
+          } else {
+            //登陆成功跳转
+            wx.showModal({
+              content: query_clone.message,
+              duration: 2000
+            })
+            that.setData({
+              haslogin: false
+            })
+            wx.setNavigationBarTitle({
+              title: '会员中心'
+            })
+            /*wx.switchTab({
+              url: '/pages/user/user'
+            })*/
+          }
+        }
+      });
+    }
+  },
+  //退出
   quit: function () {
     var that = this;
     var sessionId = wx.getStorageSync('sessionId');
@@ -20,9 +114,11 @@ Page({
       },
       method: 'GET',
       success: function (res) {
-        console.log(1);
         that.setData({
           haslogin: true
+        })
+        wx.setNavigationBarTitle({
+          title: '登录'
         })
       }
     });
@@ -34,6 +130,7 @@ Page({
     * 生命周期函数--监听页面显示
     */
   onShow: function () {
+    
     var sessionId = wx.getStorageSync("sessionId");
     var that=this;
     console.log("sessionId_transfer:" + sessionId);
@@ -50,13 +147,22 @@ Page({
           that.setData({ 
             haslogin: true
           })
+          wx.setNavigationBarTitle({
+            title: '登录'
+          })
         } else if (query_clone.flg == 0){
           that.setData({
             haslogin: true
           })
+          wx.setNavigationBarTitle({
+            title: '登录'
+          })
         }else{
           that.setData({
             haslogin: false
+          })
+          wx.setNavigationBarTitle({
+            title: '会员中心'
           })
         }
         that.setData({
